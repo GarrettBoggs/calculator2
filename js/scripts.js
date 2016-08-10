e9e4a96e1cd8437df4d84a736b4e2594365dbbc9
Kyle Lange and Yusuf Qedan <kyle@>
2016-08-10 11:52:13 -0700

    add user interface .jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,42 @@
+// UI logic
+$(document).ready(function() {
+  $("#addition form").submit(function(event) {
+    var num1 = parseInt($("input#add1").val());
+    var num2 = parseInt($("input#add2").val());
+    var answer = add(num1,num2);
+    $(".answer").text(answer);
+    $("#answer").show();
+    event.preventDefault();
+    });
+  $("#subtraction form").submit(function(event) {
+    var num1 = parseInt($("input#subtract1").val());
+    var num2 = parseInt($("input#subtract2").val());
+    var answer = subtract(num1,num2);
+    $(".answer").text(answer);
+    $("#answer").show();
+    event.preventDefault();
+    });
+    $("#multiplication form").submit(function(event) {
+      // alert()
+      var num1 = parseInt($("input#multiplication1").val());
+      var num2 = parseInt($("input#multiplication2").val());
+      answer = multiply(num1,num2);
+      $(".answer").text(answer);
+      $("#answer").show();
+      event.preventDefault();
+      });
+      $("#division form").submit(function(event) {
+        // alert()
+        var num1 = parseInt($("input#division1").val());
+        var num2 = parseInt($("input#division2").val());
+        answer = divide(num1,num2);
+        $(".answer").text(answer);
+        $("#answer").show();
+        event.preventDefault();
+        });
+  });
+
+// Biz Logic
 var add = function(number1, number2) {
   return number1 + number2;
 }
@@ -13,7 +52,3 @@ var remainder=function(num1, num2){
 var multiply = function(num1, num2){
 	return num1*num2;
 }
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
-alert (result);
