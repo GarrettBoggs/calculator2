6c2af4d5bde16f958820a4cb8774849c9bd8c5b4
Garrett Boggs and Yusuf Qedan <yusuf@>
2016-08-11 14:15:00 -0700

    update front end logic for calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,33 @@
+// Biz Logic
+var add = function(num1, num2) {
+  return num1 + num2;
+}
+var subtract = function(num1,num2){
+  return num1 - num2;
+}
+var divide = function(num1,num2){
+	return num1/num2;
+}
+var multiply = function(num1, num2){
+	return num1*num2;
+}
 // UI logic
 $(document).ready(function() {
   $("#addition form").submit(function(event) {
     var num1 = parseInt($("input#add1").val());
     var num2 = parseInt($("input#add2").val());
-    var answer = add(num1,num2);
-    $(".answer").text(answer);
-    $("#answer").show();
-    event.preventDefault();
-    });
-  $("#subtraction form").submit(function(event) {
-    var num1 = parseInt($("input#subtract1").val());
-    var num2 = parseInt($("input#subtract2").val());
-    var answer = subtract(num1,num2);
-    $(".answer").text(answer);
-    $("#answer").show();
-    event.preventDefault();
-    });
-    $("#multiplication form").submit(function(event) {
-      // alert()
-      var num1 = parseInt($("input#multiplication1").val());
-      var num2 = parseInt($("input#multiplication2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var answer;
+    if(operator === "add")
+      answer = add(num1,num2);
+    else if(operator === "subtract")
+      answer = subtract(num1,num2);
+    else if(operator === "multiply")
       answer = multiply(num1,num2);
-      $(".answer").text(answer);
-      $("#answer").show();
-      event.preventDefault();
-      });
-      $("#division form").submit(function(event) {
-        // alert()
-        var num1 = parseInt($("input#division1").val());
-        var num2 = parseInt($("input#division2").val());
-        answer = divide(num1,num2);
-        $(".answer").text(answer);
-        $("#answer").show();
-        event.preventDefault();
-        });
+    else // divide
+      answer = divide(num1,num2);
+    $(".answer").text(answer);
+    $("#answer").show();
+    event.preventDefault();
+    });
   });
-
-// Biz Logic
-var add = function(number1, number2) {
-  return number1 + number2;
-}
-var subtract = function(num1,num2){
-  return num1 - num2;
-}
-var divide=function(num1,num2){
-	return num1/num2;
-}
-var remainder=function(num1, num2){
-	return num1%num2;
-}
-var multiply = function(num1, num2){
-	return num1*num2;
-}
